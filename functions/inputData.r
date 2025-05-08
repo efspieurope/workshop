345e0ebb53a8aa79ef6929d4da3fc465b4adc47c
Kaspar Rufibach <M335337@one.merckgroup.com>
2025-05-08 17:26:15 +0200

    upate+

---
+++ functions/inputData.r
@@ -0,0 +1,24 @@
+inputData <- function(path = getwd(), regtext = "Registration"){
+
+  require(readxl)
+  require(dplyr)
+  
+  data <- as.data.frame(read_excel(paste(path, "/data/data.xlsx", sep = ""), col_types = NULL))
+  
+  # current year
+  current <- as.numeric(data %>% filter(Current == 1, na.rm = TRUE) %>% select(Year))
+  data <- data[data[, "Year"] == current, ]
+
+  # draft program with date
+  data["program"] <- paste("[Draft program (status: ", data[, "prog_version"], ")](", data[, "Program"], ")", sep = "")
+  
+  # link to registration
+  data["registration"] <- paste("[", regtext, "](", data[, "Registration"], ")", sep = "")
+  
+  # ASA FDA workshop
+  data["asafda"] <- paste("[2025 ASA Biopharmaceutical Section Regulatory-Industry Statistics Workshop](", data[, "asafda"], ")", sep = "")
+  
+  colnames(data) <- tolower(colnames(data))
+  
+  return(data)
+}
