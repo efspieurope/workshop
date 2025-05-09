b7b9e0dbc682a624395dd1468d8ee62b4442c90e
Kaspar Rufibach <M335337@one.merckgroup.com>
2025-05-09 11:27:15 +0200

    update

--- functions/inputData.r
+++ functions/inputData.r
@@ -4,24 +4,24 @@ inputData <- function(path = getwd(), regtext = "Registration"){
   require(dplyr)
   
   data <- as.data.frame(read_excel(paste(path, "/data/data.xlsx", sep = ""), col_types = NULL))
+  colnames(data) <- tolower(colnames(data))
   
   # current year
-  current <- as.numeric(data %>% filter(Current == 1, na.rm = TRUE) %>% select(Year))
-  data <- data[data[, "Year"] == current, ]
+  current <- as.numeric(data %>% filter(current == 1, na.rm = TRUE) %>% select(year))
+  data <- data[data[, "year"] == current, ]
 
   # draft program with date
-  data["program"] <- paste("[Draft program (status: ", data[, "prog_version"], ")](", data[, "Program"], ")", sep = "")
+  data["program_workshop"] <- paste("10 EFSPI regulatory statistics workshop ([draft program, status: ", data[, "prog_version"], "](", data[, "program"], "))", sep = "")
+  data["program"] <- paste("[Draft program (status: ", data[, "prog_version"], ")](", data[, "program"], ")", sep = "")
   
   # link to registration
-  data["Registration"] <- paste("[", regtext, "](", data[, "Registration"], ")", sep = "")
+  data["registration"] <- paste("[", regtext, "](", data[, "registration"], ")", sep = "")
   
   # ASA FDA workshop
   data["asafda"] <- paste("[2025 ASA Biopharmaceutical Section Regulatory-Industry Statistics Workshop](", data[, "asafda"], ")", sep = "")
   
   # chair with mail
   data["chairmail"] <- paste("[", data["chair"], "](mailto:", data["chairmail"], ")", sep = "")
-  
-  colnames(data) <- tolower(colnames(data))
   
   return(data)
 }
