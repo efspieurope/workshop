9c5d74ae4a5bb2f54a1228e58c774bfa6b60e3dc
Kaspar Rufibach <M335337@one.merckgroup.com>
2025-10-05 10:03:48 +0200

    update

--- functions/inputData.r
+++ functions/inputData.r
@@ -1,4 +1,4 @@
-inputData <- function(path = getwd(), y = 2025){
+inputData <- function(path = getwd(), y = 2026){
 
   require(readxl)
   require(dplyr)
@@ -12,14 +12,12 @@ inputData <- function(path = getwd(), y = 2025){
   data <- data[data[, "year"] == current, ]
 
   # draft program with date
-  data["issue"] <- "10th EFSPI regulatory statistics workshop"
   data["program_workshop"] <- paste(data["issue"], " ([draft program, status: ", data[, "prog_version"], "](", data[, "program"], "))", sep = "")
   data["program"] <- paste("[Draft program (status: ", data[, "prog_version"], ")](", data[, "program"], ")", sep = "")
   
   # link to registration
   data["reg_form"] <- paste("[this form](", data[, "registration"], ")", sep = "")
   data["registration"] <- paste("[Registration](", data[, "registration"], ")", sep = "")
-  data["registration"] <- "Registration is closed"
   
   # ASA FDA workshop
   data["asafda"] <- paste("[2025 ASA Biopharmaceutical Section Regulatory-Industry Statistics Workshop](", data[, "asafda"], ")", sep = "")
@@ -27,6 +25,11 @@ inputData <- function(path = getwd(), y = 2025){
   # chair with mail
   data["chairmail"] <- mailmerge(data["chair"], data["chairmail"])
   data["cashiermail"] <- mailmerge(data["cashier"], data["cashiermail"])
-  
+
+  # intermezzo
+  data["program_workshop"] <- data["issue"]
+  data["program"] <- "Program is not available yet"
+  data["registration"] <- "Registration is not open yet"
+    
   return(data)
 }
